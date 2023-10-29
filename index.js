f32ea3cd7078fd1db13b5723eef0a7a09bb6d826
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-10-29 19:49:04 +0600

    booking post

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware 
 app.use(cors())
@@ -24,6 +24,53 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    
+    const serviceCollection = client.db("CarDoctor").collection("services");
+    const teamCollection = client.db("CarDoctor").collection("team");
+    const productsCollection = client.db("CarDoctor").collection("products");
+    const bookingsCollection = client.db("CarDoctor").collection("bookings");
+
+
+    app.post('/boooking', async(req, res) =>{
+        const booking = req.body;
+        console.log(booking);
+        const result = await bookingsCollection.insertOne(booking);
+        res.send(result);
+    })
+
+    app.get('/services', async(req, res) => {
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/services/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const options = {
+            
+            // Include only the `title` and `imdb` fields in each returned document
+            projection: {  title: 1, price: 1, service_id:1 },
+          };
+        const result = await serviceCollection.findOne(query, options);
+        res.send(result);
+       
+    })
+
+    app.get('/team', async(req, res) => {
+        const cursor = teamCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/products', async(req, res) => {
+        const cursor = productsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
